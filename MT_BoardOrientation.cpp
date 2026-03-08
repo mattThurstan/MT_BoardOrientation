d51352a467a098e731ec3850e661b6a5ba0b18a9
MTS Standish <16389869+mattThurstan@users.noreply.github.com>
2026-03-08 19:04:47 +0000

    Move to IMU agnostic.

--- MT_BoardOrientation.cpp
+++ MT_BoardOrientation.cpp
@@ -36,32 +36,37 @@
  * eg. MT_BoardOrientation _orient;
  */
 MT_BoardOrientation::MT_BoardOrientation() {
-	_diSize = 10;
-	
 	/*----------------------------direction------------------------------*/
+	_diSize = 10;
+	_diThreshold = 100.00;
 	_directionCur = 0;                 			// 0=forward, 1=back, 2=up, 3=down, 4=left, 5=right, 6=stationary
 
 	/*----------------------------orientation----------------------------*/
-	_orientation = 0;        					//0=flat, 1=upside-down, 2=up, 3=down, 4=left-side, 5=right-side
+	_orientationCur = 0;        					//0=flat, 1=upside-down, 2=up, 3=down, 4=left-side, 5=right-side
 	orMatrix[0] = 0;             	    		//TEMP x =  0(low) / 1(mid) / 2(hi) - wanted to use -1, 0, 1 but too convoluted -- XYZ timed
 	orMatrix[1] = 0;
 	orMatrix[2] = 0;
 	_orOrientationSave = 0;                		//used to hold the orientation during comparison
-	_orOrientationTemp = 0;                  	//used to hold the orientation (then convert to _orientation)
+	_orOrientationTemp = 0;                  	//used to hold the orientation (then convert to _orientationCur)
 	orFlag = false;                           	//flag 0 x
 	orCounter = 0;                      		//TEMP time
+	
+	/*----------------------------indicators-----------------------------*/
+	_indSize = 10;
+	_indThreshold = 200.00;
+	_indicatorCur = 0;								//0=null, 1=Left, 2=Right
 }
 
 /*
  * Call from setup to initialise.
  */
-void MT_BoardOrientation::Init() {
+void MT_BoardOrientation::InitWithVars(uint8_t accelYAverageSize, float accelThreshold, uint8_t indYAverageSize, float indThreshold) { 
+	_diSize = accelYAverageSize;
+	_diThreshold = accelThreshold;
+	_indSize = indYAverageSize;
+	_indThreshold = indThreshold;
 	diInit();
-}
-
-void MT_BoardOrientation::InitWithVars(uint8_t accelYAverageSize) { 
-	_diSize = accelYAverageSize
-	diInit();
+	indInit();
 }
 
 /*
@@ -71,6 +76,11 @@ void MT_BoardOrientation::diInit() {
     _diAvr = (float*) malloc(_diSize * sizeof(float));
     if (_diAvr == NULL) _diSize = 0;
     diClear();
+}
+void MT_BoardOrientation::indInit() {
+    _indAvr = (float*) malloc(_indSize * sizeof(float));
+    if (_indAvr == NULL) _indSize = 0;
+    indClear();
 }
 
 /*
@@ -82,9 +92,16 @@ void MT_BoardOrientation::diClear() {
     _diSum = 0.0;
     for (int i = 0; i< _diSize; i++) _diAvr[i] = 0.0;  // needed to keep addValue simple
 }
+void MT_BoardOrientation::indClear() {
+	_indCnt = 0;
+    _indIdx = 0;
+    _indSum = 0.0;
+    for (int i = 0; i< _indSize; i++) _indAvr[i] = 0.0;  // needed to keep addValue simple
+}
 
-void MT_BoardOrientation::AddToAverageAccelY(float accelY) {
+void MT_BoardOrientation::AddToAverage(float accelY, float roll) {
 	diAddValue(accelY);
+	indAddValue(roll);
 }
 
 /*
@@ -99,6 +116,15 @@ void MT_BoardOrientation::diAddValue(float f) {
     if (_diIdx == _diSize) _diIdx = 0;  // faster than %
     if (_diCnt < _diSize) _diCnt++;
 }
+void MT_BoardOrientation::indAddValue(float f) {
+	if (_indAvr == NULL) return;
+    _indSum -= _indAvr[_indIdx];
+    _indAvr[_indIdx] = f;
+    _indSum += _indAvr[_indIdx];
+    _indIdx++;
+    if (_indIdx == _indSize) _indIdx = 0;  // faster than %
+    if (_indCnt < _indSize) _indCnt++;
+}
 
 /*
 * Get average (result) from direction rolling average array.
@@ -107,11 +133,15 @@ float MT_BoardOrientation::diGetAverage() {
 	if (_diCnt == 0) return NAN;
     return _diSum / _diCnt;
 }
+float MT_BoardOrientation::indGetAverage() {
+	if (_indCnt == 0) return NAN;
+    return _indSum / _indCnt;
+}
 
 /*
 * Called from a timed loop within the main loop.
 */
-byte MT_BoardOrientation::GetDirection() {
+void MT_BoardOrientation::DoUpdateDirection() {
 	//only call if tracking sub-mode is actually running, otherwise waste of processing..
     if (diGetAverage() > _diThreshold) {	//100
 	  _directionCur = 0;  //going forwards
@@ -126,20 +156,19 @@ byte MT_BoardOrientation::GetDirection() {
 	DEBUG_PRINTF("_directionCur ");
 	DEBUG_PRINT(_directionCur);
 	DEBUG_PRINTLNF(".");
-	
-	return _directionCur;
 }
 
 /*
 * Called from a timed loop within the main loop.
 */
-byte MT_BoardOrientation::GetOrientation(float x, float y, float z) {
+void MT_BoardOrientation::DoUpdateOrientation(float x, float y, float z) {
 	float xyz[3]; //this is a smooth gyro and accel combined to give angle
 	xyz[0] = x;
 	xyz[1] = y;
 	xyz[2] = z;
+	
     float cutoff = 45;  //starting at zero calibration, we need to know 90deg either way, so 45 is halfway point anywhere from 0
-    
+	
 	for (int i = 0; i < 3; i++) {
       if ( xyz[i] < -cutoff ) { orMatrix[i] = 0; }
       else if ( xyz[i] < cutoff && xyz[i] > -cutoff ) { orMatrix[i] = 1; }
@@ -182,16 +211,30 @@ byte MT_BoardOrientation::GetOrientation(float x, float y, float z) {
           //is the orientation still the same as when we took a sample and set the timer?
           //if so, set the actual orientation
 		  //this is all probably overkill
-          _orientation = _orOrientationTemp;
+          _orientationCur = _orOrientationTemp;
         }
         orFlag = false; //either way, reset
       }
     }
 
 	DEBUG_PRINT_HEADER();
-	DEBUG_PRINTF("_orientation ");
-	DEBUG_PRINT(_orientation);
+	DEBUG_PRINTF("_orientationCur ");
+	DEBUG_PRINT(_orientationCur);
 	DEBUG_PRINTLNF(".");
+}
+
+void MT_BoardOrientation::DoUpdateIndicator() {
+	if (indGetAverage() < -_indThreshold) {	//100
+	  _indicatorCur = 1;  //turning Left
+    } else if (diGetAverage() > _indThreshold) {
+	  _indicatorCur = 2;  //turning Right
+	} else {
+	  _indicatorCur = 0;  //stationary
+	}
+	indClear();
 	
-	return _orientation;
+	DEBUG_PRINT_HEADER();
+	DEBUG_PRINTF("_indicatorCur ");
+	DEBUG_PRINT(_indicatorCur);
+	DEBUG_PRINTLNF(".");
 }
--- MT_BoardOrientation.h
+++ MT_BoardOrientation.h
@@ -1,6 +1,6 @@
 /*
  * MT_BoardOrientation.h - Orientation setup library for long/short/skate boards. Used in longboardLight1 project.
- * Calculates direction and orientation.
+ * Calculates direction, orientation and indicators.
  * MTS Standish (mattThurstan), 2026.
  * Copyleft.
  */
@@ -30,33 +30,51 @@ class MT_BoardOrientation
     float   _diSum;
     float * _diAvr;
 	
+    uint8_t _indSize;// = 10;							//rolling average of Roll (Y) values
+    uint8_t _indCnt;
+    uint8_t _indIdx;
+    float   _indSum;
+    float * _indAvr;
+	
   private:
+	/*----------------------------direction------------------------------*/
 	void diInit();
 	void diClear();
     void diAddValue(float);
 	float diGetAverage();
-	const float _diThreshold = 100.00;				//threshold tolerance for 'dead zone' at center of readings
+	float _diThreshold;				//threshold tolerance for 'dead zone' at center of readings
 	
-	/*----------------------------direction------------------------------*/
 	byte _directionCur;                				// 0=forward, 1=back, 2=up, 3=down, 4=left, 5=right, 6=stationary
 
 	/*----------------------------orientation----------------------------*/
-	byte _orientation;                  			//0=flat, 1=upside-down, 2=up, 3=down, 4=left-side, 5=right-side
+	byte _orientationCur;                  			//0=flat, 1=upside-down, 2=up, 3=down, 4=left-side, 5=right-side
 	byte orMatrix[3];           					//TEMP x =  0(low) / 1(mid) / 2(hi)       - wanted to use -1, 0, 1 but too convoluted    -- XYZ timed
 	byte _orOrientationSave;             			//used to hold the orientation during comparison
-	byte _orOrientationTemp;           				//used to hold the orientation (then convert to _orientation)
+	byte _orOrientationTemp;           				//used to hold the orientation (then convert to _orientationCur)
 	boolean orFlag;                     			//flag 0 x
 	unsigned long orCounter;            			//TEMP time
 	const unsigned long orInterval = 450;			//interval at which to check whether flags have changed - are we still in the same orientation - how long to trigger
 	
+	/*----------------------------indicators-----------------------------*/
+	void indInit();
+	void indClear();
+    void indAddValue(float);
+	float indGetAverage();
+	float _indThreshold;					//roll deadzone. when to turn on left/right leaning/turning indicators.
+	
+	byte _indicatorCur;								//0=null, 1=Left, 2=Right
+	
   public:
 	MT_BoardOrientation();
-	void Init();
-	void InitWithVars(uint8_t accelYAverageSize);	//Y acceleration rolling average loop size
+	void InitWithVars(uint8_t accelYAverageSize, float accelThreshold, uint8_t indYAverageSize, float indThreshold);	//Y acceleration rolling average loop size, Roll (Y)
+	void AddToAverage(float accelY, float roll);	//call this something like '_diSize' times per second (currently 10). this would give a rolling average of 10 per sec.
+	void DoUpdateDirection();
+	void DoUpdateOrientation(float x, float y, float z);
+	void DoUpdateIndicator();
 	
-	void AddToAverageAccelY(float accelY);	//call this something like '_diSize' times per second (currently 10). this would give a rolling average of 10 per sec.
-	byte GetDirection();
-	byte GetOrientation(float x, float y, float z);
+	byte GetDirection() { return _directionCur; }
+	byte GetOrientation() { return _orientationCur; }
+	byte GetIndicator() { return _indicatorCur; }	//roll. used with deadzone for Left/Right orange indicators. 0=null, 1=Left, 2=Right
 };
 
 #endif
